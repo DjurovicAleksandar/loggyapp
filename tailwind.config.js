6a0ad546c36b6eabd07ac1553a2589da5cbaa8a5
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-12-22 13:41:32 +0100

    Inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgColor: "#f6f3ec",
+        primary: "#ea7210",
+      },
+      padding: {
+        padY: "74px",
+        padX: "140px",
+      },
+    },
+  },
+  plugins: [],
+};
