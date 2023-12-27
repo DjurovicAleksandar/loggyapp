b655a3867a9eb8a5a47c5430b601b844416cc1ed
DjurovicAleksandar <djur.aleksandar@gmail.com>
2023-12-27 17:20:33 +0100

    Dynamic services inside Navbar

--- src/components/navigation/NavServices.tsx
+++ src/components/navigation/NavServices.tsx
@@ -1,50 +1,40 @@
-import map from "@/assets/images/nav/map.png";
-import app from "@/assets/images/nav/app.png";
-import maintenance from "@/assets/images/nav/maintenance.png";
-import cms from "@/assets/images/nav/cms.png";
-import ux from "@/assets/images/nav/ux.png";
-import coding from "@/assets/images/nav/coding.png";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import NavServicesList from "./NavServicesList";
 
-const recentBlogPosts = [
-  {
-    blogName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 1",
-    href: "#",
-  },
-  {
-    blogName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 2",
-    href: "#",
-  },
-  {
-    blogName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 3",
-    href: "#",
-  },
-  {
-    blogName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 4",
-    href: "#",
-  },
-  {
-    blogName: "Lorem ipsum dolor sit amet consectetur adipisicing elit. 5",
-    href: "#",
-  },
-];
-
 const NavServices = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchLatestPosts = async () => {
+      try {
+        const response = await fetch("/api/posts");
+        if (!response.ok) {
+          throw new Error("Failed to fetch latest posts");
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.error("Error fetching latest posts:", error);
+      }
+    };
+
+    fetchLatestPosts();
+  }, []);
+
   return (
-    <div className=" absolute w-[700px]  bg-white z-10 -bottom-[650px] left-1/2  -translate-x-1/2 shadow-md rounded-xl">
+    <div className=" absolute w-[700px]  bg-white z-10 -bottom-[680px] left-1/2  -translate-x-1/2 shadow-md rounded-xl">
       <NavServicesList />
       <div className="bg-gray-50 px-padXMobile py-padYMobile">
         <h2 className="mb-4 font-bold">Recent Posts</h2>
         <div className="flex flex-col gap-2">
-          {recentBlogPosts.map(({ blogName, href }, i) => {
+          {posts.map(({ slug, title }, i) => {
             return (
               <a
-                href={href}
+                href={`/blog/${slug}`}
                 key={i}
                 className="text-md font-light hover:text-primary"
               >
-                {blogName}
+                {title}
               </a>
             );
           })}
