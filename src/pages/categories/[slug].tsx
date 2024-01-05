bfc59cc7d5375683154173b682d2157f07f6b6ff
DjurovicAleksandar <djur.aleksandar@gmail.com>
2024-01-05 22:09:28 +0100

    Working on Blog

--- src/pages/categories/[slug].tsx
+++ src/pages/categories/[slug].tsx
@@ -1,8 +1,7 @@
-import { GetStaticProps, GetStaticPaths } from "next";
-import { FC } from "react";
-import Link from "next/link";
-import Image from "next/image";
-import { getAllPosts, getPostsByCategory } from "../api/postFetch";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
+import { type FC } from "react";
+import { getLatestPosts, getPostsByCategory } from "../api/postFetch";
+import BlogCard from "../blog/BlogCard";
 
 interface CategoriesProps {
   posts: {
@@ -20,71 +19,64 @@ interface CategoriesProps {
 
 const CategoriesPage: FC<CategoriesProps> = ({ posts, category }) => {
   return (
-    <main className="w-full py-[40%] md:py-[15%] px-padXMobile md:px-padX flex flex-wrap items-center justify-center gap-10">
-      <h1 className="text-4xl font-bold">{category.toUpperCase()}</h1>
+    <main className="w-full py-[40%] md:py-[15%] px-padXMobile md:px-padX">
+      <h1 className="text-4xl lg:text-6xl mb-14 ">{category.toUpperCase()}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {posts.map(({ blogFront, slug }) => (
-          <div key={slug} className="p-4 border rounded-md">
-            <Image
-              src={blogFront.blogImage}
-              alt={blogFront.title}
-              width={320}
-              height={320}
-              className="rounded-xl w-[20rem] h-[20rem] mb-2"
+        {posts.map(({ blogFront, slug }) => {
+          const { title, date, shortDescription, blogImage } = blogFront;
+          return (
+            <BlogCard
+              key={slug}
+              title={title}
+              date={date}
+              shortDescription={shortDescription}
+              slug={slug}
+              blogImage={blogImage}
             />
-            <p className="text-gray-500 mb-4">{blogFront.date}</p>
-            <h2 className="text-xl font-bold mb-2">{blogFront.title}</h2>
-            <p className="mb-10 text-gray-500">{blogFront.shortDescription}</p>
-            <Link
-              style={{
-                background: "#ea7210",
-                padding: "10px",
-                borderRadius: "12px",
-                color: "white",
-              }}
-              href={`/blog/${slug}`}
-            >
-              Read more
-            </Link>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </main>
   );
 };
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const allPosts = getAllPosts();
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   const latestPosts = getLatestPosts();
 
-  // Extract categories from all posts
-  const categories = allPosts.map((post) =>
-    post.blogFront.category.toLowerCase()
-  );
+//   const paths = Object.keys(latestPosts).map((category) => ({
+//     params: { slug: category.toLowerCase() },
+//   }));
 
-  // Deduplicate and create paths for each category
-  const uniqueCategories = Array.from(new Set<string>(categories));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// };
 
-  const paths = uniqueCategories.map((category) => ({
-    params: { slug: category },
-  }));
+// export const getStaticProps: GetStaticProps = async ({ params }) => {
+//   const { slug }: any | string = params ?? { slug: "" };
 
-  return {
-    paths,
-    fallback: false,
-  };
-};
+//   const posts = getPostsByCategory(slug);
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { slug } = params ?? { slug: "" };
+//   return {
+//     props: {
+//       posts,
+//       category: slug,
+//     },
+//   };
+// };
 
-  const categorySlug = Array.isArray(slug) ? slug[0] : slug || "";
+export const getServerSideProps: GetServerSideProps<CategoriesProps> = async ({
+  params,
+}) => {
+  const { slug } = params || { slug: "" }; // Ensure params exist before accessing
 
-  const posts = getPostsByCategory(categorySlug);
+  const posts = getPostsByCategory(slug as string, 0, 6); // Ensure slug is treated as a string
 
   return {
     props: {
       posts,
-      category: slug,
+      category: slug as string, // Ensure category is treated as a string
     },
   };
 };
